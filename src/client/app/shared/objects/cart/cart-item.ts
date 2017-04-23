2d903bdef7b80d863f1c731dbbf6657b9e72d4cc
Roel <mangelschotsroel@hotmail.com>
2017-04-23 23:50:50 +0200

    cart objects

---
+++ src/client/app/shared/objects/cart/cart-item.ts
@@ -0,0 +1,5 @@
+export class CartItem {
+  id: number;
+  statusCode: string;
+  productId: number;
+}
---
+++ src/client/app/shared/objects/cart/shopping-cart.ts
@@ -0,0 +1,7 @@
+import { CartItem } from './cart-item';
+export class ShoppingCart {
+  id: number;
+  beachId: number;
+  items: CartItem[];
+  status: string;
+}
