e11605c1576984cce2ce285e5ec2b079998d7169
Roel <mangelschotsroel@hotmail.com>
2017-05-08 00:53:38 +0200

    add cart dates

--- src/client/app/shared/objects/cart/shopping-cart.ts
+++ src/client/app/shared/objects/cart/shopping-cart.ts
@@ -1,9 +1,13 @@
 import { CartItem } from './cart-item';
+import { DateTime } from '../datetime/datetime';
 export class ShoppingCart {
   constructor(public id: number = -1,
               public beachId: number,
               public items: CartItem[] = [],
               public status: string = null,
-              public price: number = 0) {
+              public price: number = 0,
+              public orderDateTime: DateTime = null,
+              public requestedDateTime: DateTime = null,
+              public deliveredDateTime: DateTime = null) {
   }
 }
---
+++ src/client/app/shared/objects/datetime/datetime.ts
@@ -0,0 +1,12 @@
+export interface DateTime {
+  dayOfMonth: number;
+  dayOfWeek: string;
+  dayOfYear: number;
+  month: string;
+  monthValue: number;
+  year: number;
+  hour: number;
+  minute: number;
+  nano: number;
+  second: number;
+}
