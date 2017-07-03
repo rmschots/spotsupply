d7d099a76b581cb861d8d7390b876c11a8b44c1b
Roel <mangelschotsroel@hotmail.com>
2017-07-03 14:44:24 +0200

    fix please-wait.js

--- tools/tasks/seed/build.assets.prod.ts
+++ tools/tasks/seed/build.assets.prod.ts
@@ -28,8 +28,7 @@ export = () => {
     '!' + join(Config.APP_SRC, '**', '*.css'),
     '!' + join(Config.APP_SRC, '**', '*.html'),
     '!' + join(Config.APP_SRC, '**', '*.scss'),
-    '!' + join(Config.APP_SRC, '**', '*.sass'),
-    '!' + join(Config.ASSETS_SRC, '**', '*.js')
+    '!' + join(Config.APP_SRC, '**', '*.sass')
   ].concat(Config.TEMP_FILES.map((p) => { return '!' + p; })))
     .pipe(onlyDirs(es))
     .pipe(gulp.dest(Config.APP_DEST));
