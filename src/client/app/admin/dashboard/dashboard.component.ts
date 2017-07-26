e3f8e7f85b6de164c6a934db7b5e4880ac5bc648
Roel <mangelschotsroel@hotmail.com>
2017-07-26 08:17:19 +0200

    admin dashboard

--- src/client/app/admin/dashboard/dashboard.component.ts
+++ src/client/app/admin/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RestGatewayService } from '../../shared/services/gateway/rest-gateway.service';
 import { URLSearchParams } from '@angular/http';
 import { MdDialog, MdSnackBar } from '@angular/material';
 import { LoadingDialogComponent } from '../../shared/components/loading/loading-dialog.component';
+import { Statistics } from '../../shared/objects/admin/statistics';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 @Component({
   moduleId: module.id,
@@ -10,8 +12,9 @@ import { LoadingDialogComponent } from '../../shared/components/loading/loading-
   templateUrl: 'dashboard.component.html',
   styleUrls: ['dashboard.component.css']
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
 
+  statistics$: BehaviorSubject<Statistics[]> = new BehaviorSubject(null);
   amountOfUsers: number = 100;
   amountOfOrders: number = 100;
 
@@ -20,8 +23,16 @@ export class DashboardComponent {
               private _snackBar: MdSnackBar) {
   }
 
+  get latestStatistics$() {
+    return this.statistics$.map(st => !!st ? st[0] : null);
+  }
+
+  ngOnInit(): void {
+    this.getStatistics();
+  }
+
   addDummyUsers() {
-    const dialogRef = this._dialog.open(LoadingDialogComponent, { data: 'Adding dummy users'});
+    const dialogRef = this._dialog.open(LoadingDialogComponent, { data: 'Adding dummy users' });
     let params: URLSearchParams = new URLSearchParams();
     params.set('amount', '' + this.amountOfUsers);
     this._restGateway.get('/testData/addDummyUsers', params)
@@ -29,7 +40,7 @@ export class DashboardComponent {
       .subscribe(() => {
         console.log('success');
         dialogRef.close();
-        this.doSnackBar('added '+this.amountOfUsers+' dummy users');
+        this.doSnackBar('added ' + this.amountOfUsers + ' dummy users');
       }, (error: Error) => {
         console.log('error');
         dialogRef.close();
@@ -38,7 +49,7 @@ export class DashboardComponent {
   }
 
   addDummyOrders() {
-    const dialogRef = this._dialog.open(LoadingDialogComponent, { data: 'Adding dummy orders'});
+    const dialogRef = this._dialog.open(LoadingDialogComponent, { data: 'Adding dummy orders' });
     let params: URLSearchParams = new URLSearchParams();
     params.set('amount', '' + this.amountOfOrders);
     this._restGateway.get('/testData/addDummyOrders', params)
@@ -46,17 +57,37 @@ export class DashboardComponent {
       .subscribe(() => {
         console.log('success');
         dialogRef.close();
-        this.doSnackBar('added '+this.amountOfUsers+' dummy users');
-        this._snackBar.open('added '+this.amountOfOrders+' dummy orders', null, {
-          duration: 2000,
-        });
+        this.doSnackBar('added ' + this.amountOfUsers + ' dummy users');
       }, (error: Error) => {
         console.log('error');
         dialogRef.close();
-        this.doSnackBar('added '+this.amountOfUsers+' dummy users');
-        this._snackBar.open('failed adding orders', null, {
-          duration: 2000,
-        });
+        this.doSnackBar('added ' + this.amountOfUsers + ' dummy users');
+      });
+  }
+
+  refreshStatistics() {
+    const dialogRef = this._dialog.open(LoadingDialogComponent, { data: 'Recalculating statistics of today' });
+    this._restGateway.get('/statistics/refresh')
+      .take(1)
+      .subscribe(data => {
+        let allStatistics = data.json() as Statistics[];
+        this.statistics$.next(allStatistics);
+        dialogRef.close();
+      }, () => {
+        this.statistics$.next(null);
+        dialogRef.close();
+        this.doSnackBar('failed recalculating statistics of today');
+      });
+  }
+
+  private getStatistics() {
+    this._restGateway.get('/statistics')
+      .take(1)
+      .subscribe(data => {
+        let allStatistics = data.json() as Statistics[];
+        this.statistics$.next(allStatistics);
+      }, () => {
+        this.statistics$.next(null);
       });
   }
 
---
+++ src/client/app/shared/objects/admin/statistics.ts
@@ -0,0 +1,8 @@
+export class Statistics {
+  public amountOfOrders: number;
+  public amountOfUsers: number;
+  public amountOfProducts: number;
+  public amountOfOrderedOrders: number;
+  public averageCartPrice: number;
+  public averageProductsPerCart: number;
+}
