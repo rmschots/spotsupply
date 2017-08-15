a681a4e35e6ecf63c56ca1574cf122a7649d6d98
Roel <mangelschotsroel@hotmail.com>
2017-08-16 01:14:44 +0200

    tmp test fix

--- spotsupply-e2e/src/test/java/be/spotsupply/e2e/tests/customer/CreateOrderEndToEndTest.java
+++ spotsupply-e2e/src/test/java/be/spotsupply/e2e/tests/customer/CreateOrderEndToEndTest.java
@@ -1,12 +1,16 @@
 package be.spotsupply.e2e.tests.customer;
 
 import be.spotsupply.e2e.page.tests.pages.CurrentOrderPage;
+import be.spotsupply.service.DeliveryService;
 import org.junit.Test;
 
 public class CreateOrderEndToEndTest extends CustomerSelenideEndToEndTest {
 
+    private DeliveryService deliveryService = new DeliveryService();
+
     @Test
     public void createOrder() {
+
         int amountOfProducts = 50;
 
         CurrentOrderPage orderInfoPage = homePage
@@ -19,7 +23,7 @@ public class CreateOrderEndToEndTest extends CustomerSelenideEndToEndTest {
             .assertTotalPrice(amountOfProducts * 2.5)
             .clickNextButton()
             .assertTotalPrice(amountOfProducts * 2.5)
-            .assertDeliverytime("ASAP")
+            .assertDeliverytime(deliveryService.getDeliveryTimes().get(0))
             .clickBringitButton()
             .confirm()
             .continueToCurrentOrderPage();
